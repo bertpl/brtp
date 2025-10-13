c08febfdff2994b041a3aa73ffff02b977b1facd
Bert Pluymers <bert.pluymers@gmail.com>
2025-10-13 19:39:49 +0200

    Implement zip_random.

---
+++ brtp/misc/utils/__init__.py
@@ -0,0 +1,1 @@
+from ._zip_random import zip_random
---
+++ tests/misc/utils/test_zip_random.py
@@ -0,0 +1,37 @@
+import pytest
+
+from brtp.misc.utils import zip_random
+
+
+@pytest.mark.parametrize("seed", [None, 0, 42, 1234])
+def test_zip_random_invariants(seed: int | None):
+    # --- arrange -----------------------------------------
+    i1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    i2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+    # --- act ---------------------------------------------
+    result = list(zip_random(i1, i2, seed=seed))
+
+    # --- assert ------------------------------------------
+    assert {v for v, _ in result} == set(i1)
+    assert [v for v, _ in result] != i1
+    assert {v for _, v in result} == set(i2)
+    assert [v for _, v in result] != i2
+
+
+def test_zip_random_seed():
+    # --- arrange -----------------------------------------
+    i1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    i2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+    # --- act ---------------------------------------------
+    result_1 = list(zip_random(i1, i2, seed=None))
+    result_2 = list(zip_random(i1, i2, seed=1))
+    result_3 = list(zip_random(i1, i2, seed=2))
+    result_4 = list(zip_random(i1, i2, seed=2))
+
+    # --- assert ------------------------------------------
+    assert result_1 != result_2
+    assert result_1 != result_3
+    assert result_2 != result_3
+    assert result_3 == result_4
